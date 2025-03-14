7723877ed0c9b60a4546e81254f517a957f6f460
raya <2pitraulia5@gmail.com>
2025-03-14 11:10:11 +0800

    memperbarui konfigurasi

--- rayadanaga/urls.py
+++ rayadanaga/urls.py
@@ -1,10 +1,23 @@
-from django.contrib import admin
-from django.urls import path
-
-
 from rayadanaga.views import home, about
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from user.views import (
+    CategoryListCreateView, CategoryDetailView, 
+    OutfitListCreateView, OutfitDetailView,
+    SavedOutfitListCreateView, SavedOutfitDetailView
+
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('api/categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
+    path('api/outfits/', OutfitListCreateView.as_view(), name='outfit-list-create'),
+    path('api/outfits/<int:pk>/', OutfitDetailView.as_view(), name='outfit-detail'),
+    path('api/saved-outfits/', SavedOutfitListCreateView.as_view(), name='saved-outfit-list-create'),
+    path('api/saved-outfits/<int:pk>/', SavedOutfitDetailView.as_view(), name='saved-outfit-detail'),
     path('', home, name="home"),
     path('about', about, name="about")
 ]
